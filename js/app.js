e879bc8fd9fec75a93f2337c8144fa53ecfe7f55
AliDemircix <alidemircix@gmail.com>
2021-11-25 23:36:09 +0100

    JavaScript Selectors Created

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const selectDay = document.querySelector('.events__select');
+const selectHour = document.querySelector('.events__hours__select');
+const task = document.querySelector('.events__content');
+const submitTask = document.querySelector('.button');
+
+
+
